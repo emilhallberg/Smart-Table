4e9fdf13119c3c75687f37d201dfdcadc95ef404
Simon Johansson <id16sjn@cs.umu.se>
2020-01-10 08:47:36 +0100

    Implemented notification page

--- views/Notifications.jsx
+++ views/Notifications.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Bell } from '../assets/Assets';
+import Colors from '../utils/Colors';
+
 const { create } = StyleSheet;
 
 const styles = create({
   container: {
-    flex: 1
+    flex: 1,
+  },
+  headline: {
+    justifyContent: "center",
+    alignItems:"center",
+    flexDirection:"row",
+    paddingTop: 30,
+    
   }
 });
 
 const Notifications = ({ route }) => {
   return (
     <View style={styles.container}>
-      <Text>Mina notiser</Text>
+      <View style={styles.headline}>
+        <Text style={{fontSize: 25, paddingEnd:10}}>Mina notiser</Text>
+        <Bell fill={Colors.primary}/>
+      </View>
     </View>
   );
 };
